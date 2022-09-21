98c99852b6d86c345c17598161df75bd13fa0f90
Rúben Antunes <rubenantunes@bs007-imac.wygroup.net>
2022-09-21 15:56:53 +0100

    Initial project

---
+++ AcademyChallengeApp/AcademyChallengeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AcademyChallengeApp
+//
+//  Created by Rúben Antunes on 21/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

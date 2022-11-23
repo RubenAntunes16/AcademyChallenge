b524e8d67c0f85c8fbc6aa524c0f905e962effe3
Rúben Antunes <rantunes@blissapplications.com>
2022-11-23 11:54:20 +0000

    Remove UIImageView+Spinner file

---
+++ AcademyChallengeApp/AcademyChallengeApp/Utilities/PersistenceEnum.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceEnum.swift
+//  AcademyChallengeApp
+//
+//  Created by Rúben Antunes on 23/11/2022.
+//
+
+import Foundation
---
+++ AcademyChallengeApp/AcademyChallengeApp/Utilities/ServiceEnum.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceEnum.swift
+//  AcademyChallengeApp
+//
+//  Created by Rúben Antunes on 23/11/2022.
+//
+
+import Foundation
